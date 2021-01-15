fb4ece9728e3c14b6f772361daba25e5928805ac
DaatUserName <40370496+DaatUserName@users.noreply.github.com>
2021-01-15 11:15:04 +0100

    Add files via upload

---
+++ inc/footer.php
@@ -0,0 +1,11 @@
+<script src="inc/js/jquery.min.js"></script>
+<script src="inc/js/bootstrap.min.js"></script>
+<?php echo "</html>"; ?>
+<footer>
+<div class="footer">
+    <img src="https://i.imgur.com/hB1uqbo.png">
+    <p>&copy; <a href="https://vincimc.com"><strong>VinciMc</strong></a></p>
+    <p>Designed by <a href="https://github.com/DaatUserName"><strong>Tobias Jensen</strong></a></p>
+    
+</div>
+</footer>
